a131e8526010d993160ab12aa493a26bf0bf7846
BarancanS <barancansng@gmail.com>
2023-09-21 00:50:00 +0300

    Update page.js

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -43,18 +43,24 @@ const Page = () => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center text-black">
-      <h1 className="text-white text-4xl mb-4">Currency App</h1>
-      <div className="flex flex-col gap-2">
+      <h1 className="text-3xl font-semibold text-blue-800 mb-4">
+        Currency Converter
+      </h1>
+      <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
           <input
             type="number"
             ref={ref}
             value={text1}
             onChange={(e) => setText1(e.target.value)}
-            className="text-black"
+            className="p-2 border rounded-md text-lg text-blue-800"
           />
-          <select name="" id="" className="text-black" onChange={handleSelect}>
-            <option value="">Select Currency</option>
+          <select
+            name=""
+            id=""
+            className="p-2 border rounded-md text-lg text-blue-800"
+            onChange={handleSelect}
+          >
             {Object.keys(currencies).map((currency, index) => {
               return (
                 <option key={index} value={currencies[currency]}>
@@ -69,15 +75,14 @@ const Page = () => {
             type="number"
             ref={ref}
             value={totalValue}
-            className="text-black"
+            className="p-2 border rounded-md text-lg text-blue-800"
           />
           <select
             name=""
             id=""
-            className="text-black"
+            className="p-2 border rounded-md text-lg text-blue-800"
             onChange={handleSelectSecond}
           >
-            <option value="">Select Currency</option>
             {Object.keys(currencies).map((currency, index) => {
               return (
                 <option key={index} value={currencies[currency]}>
@@ -88,16 +93,16 @@ const Page = () => {
           </select>
         </div>
       </div>
-      <div className="flex flex-col gap-2 text-white">
+      <div className="flex flex-col gap-4 text-white">
         <button
           onClick={calculate}
-          className="bg-blue-800 p-2 rounded-lg hover:scale-105 transition-all duration-500"
+          className="bg-blue-800 px-4 py-2 rounded-md hover:bg-blue-600 transition-all duration-300"
         >
           Calculate
         </button>
         <button
           onClick={clear}
-          className="bg-blue-800 p-2 rounded-lg hover:scale-105 transition-all duration-500"
+          className="bg-blue-800 px-4 py-2 rounded-md hover:bg-blue-600 transition-all duration-300"
         >
           Clear
         </button>
